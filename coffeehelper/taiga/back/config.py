bf0f591bddc73c9b1cd06d477e7709bd8cfab56d
dhilipsiva <dhilipsiva@pm.me>
2019-12-31 09:49:01 +0530

    Move taiga to coffeehelper
    Because our clients needs access to it (they are not on VPN)

--- coffeehelper/taiga/back/config.py
+++ coffeehelper/taiga/back/config.py
@@ -27,18 +27,21 @@ DATABASES = {
     }
 }
 
-#DEFAULT_FROM_EMAIL = "john@doe.com"
 #CHANGE_NOTIFICATIONS_MIN_INTERVAL = 300 #seconds
-#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-#EMAIL_USE_TLS = False
-#EMAIL_USE_SSL = False # You cannot use both (TLS and SSL) at the same time!
-#EMAIL_HOST = 'localhost'
-#EMAIL_PORT = 25
-#EMAIL_HOST_USER = 'user'
-#EMAIL_HOST_PASSWORD = 'password'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+DEFAULT_FROM_EMAIL = environ['DEFAULT_FROM_EMAIL']
+EMAIL_USE_TLS = environ['EMAIL_USE_TLS'] == '1'
+EMAIL_USE_SSL = environ['EMAIL_USE_SSL'] == '1'
+EMAIL_HOST = environ['EMAIL_HOST']
+EMAIL_PORT =  int(environ['EMAIL_PORT'])
+EMAIL_HOST_USER = environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = environ['EMAIL_HOST_PASSWORD']
 
 EVENTS_PUSH_BACKEND = "taiga.events.backends.rabbitmq.EventsPushBackend"
 RABBIT_HOST=environ['RABBIT_HOST']
 RABBIT_VHOST=environ['RABBITMQ_DEFAULT_VHOST']
 EVENTS_PUSH_BACKEND_OPTIONS = {
     "url": f"amqp://guest:guest@{RABBIT_HOST}:5672/{RABBIT_VHOST}"}
+
+PUBLIC_REGISTER_ENABLED = True
+USER_EMAIL_ALLOWED_DOMAINS = ["reckonsys.com", "reckonsys.xyz"]
